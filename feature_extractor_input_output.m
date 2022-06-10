e35cfbf42dc213645edb9af673dc08fa6d1cb5d3
mgordiyenko14 <106877528+mgordiyenko14@users.noreply.github.com>
2022-06-10 12:14:31 -0400

    Add files via upload

--- feature_extractor_input_output.m
+++ feature_extractor_input_output.m
@@ -3,35 +3,59 @@
 % and another that is the spectrogram output of the feature extractor
 % function.
 
+% get the pathway
+% put all the file names into an array usein x - dir(allFiles)
+% for k = 1:length(x)
+% use *. to go through all of the files in the folder with a for loop
+% use 
+
 %% define time and generate input signal
-x = "/Users/nolantremelling/matlab/Analog Machine Learning Research/Practice Spectrogram/speech_commands_v0.02/sheila/0d393936_nohash_1.wav";
-x_preproc = fn_preprocess_audio_clip(audioread(x));
+x = "/Users/mg/Desktop/speech_commands_v0.02/sheila";
 t_start = 0; t_stop = 1; t_step = 6.25e-5;
 t = t_start:t_step:t_stop-t_step;
-%f_i = 4e3; a_i = [zeros(1,length(t)/2) , ones(1,length(t)/2)];  % sinusoid with linear envelope
-x_i = x_preproc;
+
 clear f_i a_i;
 
-%createFilterbankAndPlot = sc_create_filterbank_and_plot_response(1, 100, 4000, 3, 1);
+%generate and save images for all the files
+
+filePattern = fullfile(x,"*.wav");
+theFiles = dir(filePattern);
+
+for k=1:length(theFiles)
+    
+    currentFile = theFiles(k).name;
+    fullFileName = fullfile(theFiles(k).folder, currentFile); % get the name of the file to process
+    % preprocess the file
+    x_preproc = fn_preprocess_audio_clip(audioread(fullFileName));
+    x_i = x_preproc;
+
+    % feed input signal into feature extractor
+    x_o = fn_fex(t,x_i);
+   
+    % plot input to and output of feature extractor
+    figure;
+    
+    % input
+    subplot(2,1,1);
+    input = plot(x_preproc);
+    title('input signal');
+    xlabel('time [ms]');
+    ylabel('amplitude [#]');
+    %saveas(input,'curerntFile.jpg');
+    
+    % output
+    subplot(2,1,2);
+    output = imagesc(x_o);
+    title('output spectrogram');
+    xlabel('sample index (10ms per sample) [#]');
+    ylabel('feature index (in reverse order) [#]');
+    %saveas(output,'curerntFile.jpg');
+
+    %get the name of the file and save output as jpg
+    [filepath,name,ext] = fileparts(currentFile);
+    saveas(figure,name,'jpg');
+end
 
 
-%% feed input signal into feature extractor
-x_o = fn_fex(t,x_i,16, 100, 4000, 3, 1);
 
 
-%% plot input to and output of feature extractor
-figure;
-
-% input
-subplot(2,1,1);
-plot(x_preproc);
-title('input signal');
-xlabel('time [ms]');
-ylabel('amplitude [#]');
-
-% output
-subplot(2,1,2);
-imagesc(x_o);
-title('output spectrogram');
-xlabel('sample index (10ms per sample) [#]');
-ylabel('feature index (in reverse order) [#]');
