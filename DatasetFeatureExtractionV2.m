bb05459800e8c5944e0e39fc6a8208f2f3248519
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2022-07-25 17:33:40 -0400

    Completed implementation of the feature extractor

--- DatasetFeatureExtractionV2.m
+++ DatasetFeatureExtractionV2.m
@@ -1,14 +1,13 @@
 %% Dataset feature extraction
 % So far this script produces the desired .mat output for the feature
-% extractor. The labels are not finished, and then implementation with
-% trainTestValidate.m needs to occur. <- this file is what distributes the
-% dataset and contains sizing information.
+% extractor with labels. In the future, update to suppress warnings for
+% 'undersampled' audio files.
+
+% An additional updated version of this implementation of the feature
+% extractor should step through the feature extractor parameters to create
+% a massive dataset of processed audio.
 
 %% Define dataset source
-dataSets = "/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/Speech Command Datasets/";
-speechCommands = "/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/Speech Command Datasets/speech_commands_v0.02/";
-hardDrive = "/Volumes/NolansDrive/Processed Data";
-outputFile = "/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/Speech Command Datasets/outputmodel.mat";
 
 % In the final implementation these parameters will be given a range such
 % that they will be stepped through rather than hardcoded.
@@ -21,6 +20,12 @@ f_c_max = 4000;
 q = 3;
 a_pb = 1;
 weight = 0.005;
+
+dataSets = "/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/Speech Command Datasets/";
+speechCommands = "/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/Speech Command Datasets/speech_commands_v0.02/";
+hardDrive = "/Volumes/NolansDrive/Processed Data";
+paramFile = horzcat("N", n_filters, "_Min", f_c_min, "_Max", f_c_max, "_Q", q);
+
 
 %% Define and create directories
 clear f_i a_i;
@@ -36,8 +41,10 @@ fexFolder = horzcat('N', num2str(n_filters), '_Min', num2str(f_c_min), '_Max', n
 newDir = ['/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/' fexFolder];
 disp(newDir)
 mkdir(newDir);
+outputFile = strcat('/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/', fexFolder, "/outputmodel.mat");
 
-%% Create directories and feed images to feature extractor
+
+%% Create training set data
 for k = 1: length(folders)
     filePattern = fullfile(speechCommands, folders{k}, "/*.wav");
     theFiles = dir(filePattern);
@@ -49,7 +56,7 @@ for k = 1: length(folders)
 
 
     trainingOutput = zeros(n_rows,n_cols,n_channels,2); % Initialize post-feature extraction training set
-    for i = 1: 3
+    for i = 1: 80
         currentFile = theFiles(i).name;
         % Get name of current file
         fullFileName = fullfile(theFiles(i).folder, currentFile);
@@ -63,39 +70,87 @@ for k = 1: length(folders)
     end
 end
 
-%% Create labels
-% need to further develop this part of the script… should make a matrix
-% that will follow the pattern
-% ( 1, folder(1))
-% ( 1, folder(2))
-% ( 1, folder(3))
-% …
-% ( 105, folder(3))
-% 
-% Just have not had time to finish this
+%% Create training set labels
 
-trainingLabels = zeros(105, 2); % Hardcoded to 3*35 labels
-folderCount = 1;
+trainingLabels = {};
 
-for i = 1: 105
-    trainingLabels(i, i) = i;
+for i = 1 : length(folders)
+    for j = 1 : 80
+        trainingLabels = [trainingLabels, folders{i}];
+    end
+end
+
+%% Create testing set data
+for k = 1: length(folders)
+    filePattern = fullfile(speechCommands, folders{k}, "/*.wav");
+    theFiles = dir(filePattern);
+
+    % dimension of feature extractor
+    n_rows = n_filters;
+    n_cols = 100;
+    n_channels = 1;
+
+
+    testingOutput = zeros(n_rows,n_cols,n_channels,2); % Initialize post-feature extraction training set
+    for i = 81 : 90
+        currentFile = theFiles(i).name;
+        % Get name of current file
+        fullFileName = fullfile(theFiles(i).folder, currentFile);
+        for j=1:size(testingOutput,4)
+            % Preprocess the file
+            x_i = fn_preprocess_audio_clip(audioread(fullFileName));
+            % Run input signal in feature extractor
+            x_o = fn_fex(t_start, t_stop, x_i, n_filters, f_c_min, f_c_max, q, a_pb);
+            testingOutput(:,:,:,j) = x_o;
+        end
+    end
+end
+
+%% Create testing set labels
+
+testingLabels = {};
+
+for i = 1 : length(folders)
+    for j = 1 : 10
+        testingLabels = [testingLabels, folders{i}];
+    end
+end
+
+%% Create validation set data
+for k = 1: length(folders)
+    filePattern = fullfile(speechCommands, folders{k}, "/*.wav");
+    theFiles = dir(filePattern);
+
+    % dimension of feature extractor
+    n_rows = n_filters;
+    n_cols = 100;
+    n_channels = 1;
+
+
+    validationOutput = zeros(n_rows,n_cols,n_channels,2); % Initialize post-feature extraction training set
+    for i = 91: 100
+        currentFile = theFiles(i).name;
+        % Get name of current file
+        fullFileName = fullfile(theFiles(i).folder, currentFile);
+        for j=1:size(validationOutput,4)
+            % Preprocess the file
+            x_i = fn_preprocess_audio_clip(audioread(fullFileName));
+            % Run input signal in feature extractor
+            x_o = fn_fex(t_start, t_stop, x_i, n_filters, f_c_min, f_c_max, q, a_pb);
+            validationOutput(:,:,:,j) = x_o;
+        end
+    end
+end
+
+%% Create validation set labels
+
+validationLabels = {};
+
+for i = 1 : length(folders)
+    for j = 1 : 10
+        validationLabels = [validationLabels, folders{i}];
+    end
 end
 
 %% Save file
-save(outputFile, 'trainingOutput');
-
-%% Make copies for validation list
-validation = readlines('/Users/nolantremelling/matlab/Analog Machine Learning Research/Analog Machine Learning/Speech Command Datasets/speech_commands_v0.02/testing_list.txt');
-cd(newDir)
-mkdir 'validationList'
-
-for k = 1:length(validation)
-    valFile = strcat(newDir, '/', validation(k));
-    valDestination = strcat('/Volumes/NolansDrive/Processed Data/3210040003/validationList');
-    valName = strcat(validation(k));
-    newStr = extractAfter(valName,"/");
-    copyfile(valFile, valDestination);
-
-    cd('/Volumes/NolansDrive/Processed Data/3210040003/validationList')
-    movefile('*.jpg', valName);
-end
+save(outputFile, 'trainingOutput', 'trainingLabels', 'testingOutput', 'testingLabels', 'validationOutput', 'validationLabels');
