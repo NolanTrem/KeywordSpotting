8b86c288caf8a8cd687d45ae5861159b5bc897fa
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2022-08-26 10:44:56 -0400

    Add filter results

--- numberOfFiltersResults.m
+++ numberOfFiltersResults.m
@@ -5,12 +5,25 @@
 n2 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N2_Min100_Max_Q3_Network.mat');
 n4 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N4_Min100_Max_Q3_Network.mat');
 n6 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N6_Min100_Max_Q3_Network.mat');
+n8 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N8_Min100_Max_Q3_Network.mat');
+n10 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N10_Min100_Max_Q3_Network.mat');
+n12 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N12_Min100_Max_Q3_Network.mat');
+n14 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N14_Min100_Max_Q3_Network.mat');
+n16 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N16_Min100_Max_Q3_Network.mat');
+n18 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N18_Min100_Max_Q3_Network.mat');
+n20 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N20_Min100_Max_Q3_Network.mat');
+n22 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N22_Min100_Max_Q3_Network.mat');
 
 %% Store accuracy
-x = [2, 4, 6];
-accuracyMeasures = [n2.accuracy; n4.accuracy; n6.accuracy];
+x = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22];
+accuracyMeasures = [n2.accuracy; n4.accuracy; n6.accuracy; n8.accuracy;
+    n10.accuracy; n12.accuracy; n14.accuracy; n16.accuracy; n18.accuracy;
+    n20.accuracy; n22.accuracy];
 
-plot(x, accuracyMeasures)
-xlim([0 10])
+plot(x, accuracyMeasures, "LineWidth", 1.5)
+ylabel("Accuracy");
+xlabel("Number of filters");
+title("Classification accuracy with variated filters")
+xlim([0 24])
 
 %boxplot(accuracyMeasures, "Orientation", "horizontal")
