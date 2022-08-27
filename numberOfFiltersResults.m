7f3dc0e064687b543fa06d2123fe0ccd3c781db4
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2022-08-27 17:23:27 -0400

    Update filter results

--- numberOfFiltersResults.m
+++ numberOfFiltersResults.m
@@ -13,17 +13,23 @@ n16 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMa
 n18 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N18_Min100_Max_Q3_Network.mat');
 n20 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N20_Min100_Max_Q3_Network.mat');
 n22 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N22_Min100_Max_Q3_Network.mat');
+n24 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N24_Min100_Max_Q3_Network.mat');
+n26 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N26_Min100_Max_Q3_Network.mat');
+n28 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N28_Min100_Max_Q3_Network.mat');
+n30 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N30_Min100_Max_Q3_Network.mat');
+n32 = load('/Users/nolantremelling/matlab/AnalogMachineLearningResearch/AnalogMachineLearning/N32_Min100_Max_Q3_Network.mat');
 
 %% Store accuracy
-x = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22];
+x = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32];
 accuracyMeasures = [n2.accuracy; n4.accuracy; n6.accuracy; n8.accuracy;
     n10.accuracy; n12.accuracy; n14.accuracy; n16.accuracy; n18.accuracy;
-    n20.accuracy; n22.accuracy];
+    n20.accuracy; n22.accuracy; n24.accuracy; n26.accuracy; n28.accuracy;
+    n30.accuracy; n32.accuracy];
 
 plot(x, accuracyMeasures, "LineWidth", 1.5)
 ylabel("Accuracy");
 xlabel("Number of filters");
 title("Classification accuracy with variated filters")
-xlim([0 24])
+xlim([0 34])
 
 %boxplot(accuracyMeasures, "Orientation", "horizontal")
